8ee76d003f8c654fb4393742123ffe552250bbcb
Cynthia Jiang <xin.jiang@microsoft.com>
2024-12-03 16:51:38 -0500

    updated timeout

--- src/ContosoSuitesDashboard/pages/2_API_Integration.py
+++ src/ContosoSuitesDashboard/pages/2_API_Integration.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 def get_hotels():
     """Return a list of hotels from the API."""
     api_endpoint = st.secrets["api"]["endpoint"]
-    response = requests.get(f"{api_endpoint}/Hotels", timeout=10)
+    response = requests.get(f"{api_endpoint}/Hotels", timeout=120)
     return response
 
 @st.cache_data
